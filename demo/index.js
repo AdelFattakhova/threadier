8d83e2f01f148335ec62dc0e48e56290f5986c7f
Adel Fattakhova <adel.fattakhova.162@gmail.com>
2023-07-20 11:53:58 +0300

    allow task aborting

--- demo/index.js
+++ demo/index.js
@@ -47,7 +47,7 @@ function createTask() {
   progress.style.borderColor = COLORS[priority];
   progress.dataset.color = COLORS[priority]
 
-  scheduler.addTask(function* () {
+  return scheduler.addTask(function* () {
     for (let j = 1; j <= ITERATIONS; j++) {
       text.innerHTML = `<span>${j}</span>`;
       progress.style.width = `${j / ITERATIONS * 100}%`;
@@ -56,20 +56,32 @@ function createTask() {
   }, { priority });
 }
 
+const taskes = [];
+
 for (let i = 0; i < 48; i++) {
-  createTask();
+  taskes.push(createTask());
 }
 
 setTimeout(() => {
   for (let i = 0; i < 8; i++) {
-    createTask();
+    taskes.push(createTask());
   }
 }, 5000);
 
-scheduler.addTask(() => {
+setTimeout(() => {
+  taskes[0].abort();
+  taskes[4].abort();
+  taskes[8].abort();
+  taskes[16].abort();
+  scheduler.abortTask(taskes[24]);
+  scheduler.abortTask(taskes[20]);
+  scheduler.abortTask(taskes[12]);
+}, 5000);
+
+const wwTask = scheduler.addTask(() => {
   try {
     let count = 0;
-    for (let j = 0; j < 1e6; j++) {
+    for (let j = 0; j < 1e7; j++) {
       count++;
     }
     return Promise.resolve(count);
@@ -77,3 +89,5 @@ scheduler.addTask(() => {
     return Promise.reject(e);
   }
 }, { inWebWorker: true });
+
+scheduler.abortTask(wwTask);
