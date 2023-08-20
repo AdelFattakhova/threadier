cdb6748c883b27a17626cc41ffd814d6aa20e96a
Adel Fattakhova <adel.fattakhova.162@gmail.com>
2023-08-20 22:02:26 +0300

    start scheduler as microtask

--- src/scheduler.ts
+++ src/scheduler.ts
@@ -37,7 +37,7 @@ export class Scheduler {
     this.#tasksPipe.head.collection[task.priority].add(task);
     this.#tasksPipe.head.size++;
 
-    if (!this.#running) this.#start();
+    if (!this.#running) queueMicrotask(this.#start);
 
     return resultPromise;
   }
